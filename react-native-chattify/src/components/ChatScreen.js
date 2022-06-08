6f0da146c0c47b02118f4e1db13e0b237e75a9ec
SONKENG MBOGNING Brice <cruzorbladex@gmail.com>
2022-06-08 09:14:42 +0100

    Added the text input at the bottom of the chat with a send button

--- react-native-chattify/src/components/ChatScreen.js
+++ react-native-chattify/src/components/ChatScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {View, Text, FlatList} from 'react-native';
+import {View, FlatList, StyleSheet, TextInput, Image, TouchableOpacity} from 'react-native';
 import Bubble from "./Bubble";
 
 const coversation = [
@@ -13,10 +13,23 @@ const coversation = [
     {id:3, 'message':'Can i help you?', sender:'Metanymous', time:'9 min ago'},
     {id:1, 'message':'No it is ok. I am pretty sure i will be done before the night', sender:'Cruzor Blade', time:'5 min ago'},
     {id:2, 'message':'Ah ok. Then, have a good day ;)', sender:'Epic Suave', time:'1 min ago'},
-]
+];
+
 const ChatScreen = () => {
     return (
-        <View>
+        <View style={{flexDirection:'column'}}>
+            <View style={styles.messageView}>
+                <TextInput
+                    style={styles.msgInput}
+                    placeholder="type a message..."
+                    multiline={true}
+                />
+                <TouchableOpacity>
+                    <View style={styles.sendBtn}>
+                        <Image style={styles.sendIcon} source={require('./paperplane.png')} />
+                    </View>
+                </TouchableOpacity>
+            </View>
             <FlatList
             data={coversation}
             keyExtractor={item => item.message.toString()}
@@ -28,9 +41,55 @@ const ChatScreen = () => {
                     isCurrentUser={item.id == 1}
                 />
             }
+            style={{marginBottom:50}}
+            contentContainerStyle={{paddingBottom:10}}
             />
         </View>
     )
 }
 
 export default ChatScreen;
+
+const styles = StyleSheet.create({
+    messageView: {
+        height:60,
+        width:'100%',
+        // borderWidth:1,
+        // backgroundColor:'blue',
+        position:'absolute',
+        bottom:0,
+        flexDirection:'row',
+        paddingHorizontal:10,
+        // paddingVertical:6,
+        paddingTop:10,
+        alignItems:'center',
+    },
+    msgInput: {
+        backgroundColor:"#e3e3e3",
+        flex:5,
+        borderRadius:10,
+        paddingHorizontal:20,
+        // elevation:1,
+        marginRight:10,
+        fontSize:17,
+        alignItems:'center',
+        textAlignVertical:'center',
+        paddingBottom:5,
+        marginVertical:5
+    },
+    sendBtn: {
+        // flex:1,
+        backgroundColor:'blue',
+        alignItems:'center',
+        justifyContent:'center',
+        height:40,
+        width:42,
+        borderRadius:20
+    },
+    sendIcon: {
+        height:33,
+        width:33,
+        marginLeft:-4,
+        tintColor:'#fff'
+    }
+})
